20101204bade0c14c65d76c82a222233a1c6f631
James Mernin <jamespmernin@gmail.com>
2020-10-10 15:56:20 -0400

    Adapted for 360x640 fisplay

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function History() {
   return (
     <div className="history">
-      <h3 id="historyHeader">History</h3>
+      <h3>History</h3>
       <p id="historyList"></p>
     </div>
   );
