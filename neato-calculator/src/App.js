466d322396665a149bef70c6f30356e765747301
James Mernin <jamespmernin@gmail.com>
2020-10-10 12:01:05 -0400

    Got buttons to switch modes

--- neato-calculator/src/App.js
+++ neato-calculator/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
-import axios from 'axios';
-import './App.css';
+import { Button } from 'semantic-ui-react'
+// import axios from 'axios';
 import Display from './components/Display';
 import Standard from './components/Standard';
 import Scientific from './components/Scientific';
+import './App.css';
 
 function App() {
   return (
@@ -12,8 +13,17 @@ function App() {
       <h1>Neato Calculator</h1>
       <h2>by: James Mernin</h2>
       <nav>
-        <button className="mode">Standard</button>
-        <button className="mode">Scientific</button>
+        {/* Idea for buttons from https://stackoverflow.com/questions/42463263/wrapping-a-react-router-link-in-an-html-button */}
+        <Link to="/standard">
+          <Button className="mode">
+            <p id="standard">Standard</p>
+          </Button>
+        </Link>
+        <Link to="/scientific">
+          <Button className="mode">
+            <p id="scientific">Scientific</p>
+          </Button>
+        </Link>
       </nav>
       <Display />
       <Route path="/standard">
