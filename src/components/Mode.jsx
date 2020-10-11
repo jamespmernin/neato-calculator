54936093ec07e7ceb9a1b866ead81d34412eb2e5
James Mernin <jamespmernin@gmail.com>
2020-10-11 13:55:24 -0400

    Got the app to look nice on mobile, need help with functionality

--- src/components/Mode.jsx
+++ src/components/Mode.jsx
@@ -8,14 +8,10 @@ function Mode() {
       <nav>
         {/* Idea for buttons from https://stackoverflow.com/questions/42463263/wrapping-a-react-router-link-in-an-html-button */}
         <Link to="/mode/standard">
-          <Button className="mode">
-            <p id="standard">Standard</p>
-          </Button>
+          <Button className="mode">Standard</Button>
         </Link>
         <Link to="/mode/scientific">
-          <Button className="mode">
-            <p id="scientific">Scientific</p>
-          </Button>
+          <Button className="mode">Scientific</Button>
         </Link>
       </nav>
     </>
