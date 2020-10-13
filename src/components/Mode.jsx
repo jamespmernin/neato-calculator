9a1eb474c6a1841663a04702502dbb0c37a50309
James Mernin <jamespmernin@gmail.com>
2020-10-13 13:55:18 -0400

    Style on text boxes for mode

--- src/components/Mode.jsx
+++ src/components/Mode.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Button } from 'semantic-ui-react'
 
 function Mode() {
-  const changeModeStyle = (e) => {
-    e.target.style.textWeight = 'bold';
-  }
+  const activeStyle = {
+    boxShadow: '4px 4px 4px green'
+  };
   return (
     <>
       <nav>
         {/* Idea for buttons from https://stackoverflow.com/questions/42463263/wrapping-a-react-router-link-in-an-html-button */}
-        <Link to="/standard">
-          <Button className="mode" id="standard" onClick={changeModeStyle}>Standard</Button>
-        </Link>
-        <Link to="/scientific">
-          <Button className="mode" id="scientific" onClick={changeModeStyle}>Scientific</Button>
-        </Link>
+        <NavLink activeStyle={activeStyle} to="/standard">
+          <Button className="mode" id="standard" >Standard</Button>
+        </NavLink>
+        <NavLink activeStyle={activeStyle} to="/scientific">
+          <Button className="mode" id="scientific">Scientific</Button>
+        </NavLink>
       </nav>
     </>
   )
