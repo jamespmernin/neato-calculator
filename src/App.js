071d988767c6f5189f0c669e8201451e742bf0f0
James Mernin <jamespmernin@gmail.com>
2020-10-13 10:53:54 -0400

    Refactored switch statements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -9,158 +9,126 @@ import Scientific from './components/Scientific';
 import './App.css';
 
 function App() {
-  let number = '';
-  let phrase = '';
-  let result = '';
+  let [number, setNumber] = useState('');
+  let [phrase, setPhrase] = useState('');
+  let [result, setResult] = useState('');
+
   let helpEndPhrase = false;
 
+  const addZero = () => {
+    if (number === '') {
+      setNumber('0');
+    }
+  }
+
   const evaluate = () => {
-    result = eval(phrase); // eslint-disable-line no-eval
-    helpEndPhrase = false;
-    phrase = result;
-    number = '';
+    if (helpEndPhrase === true) {
+      setPhrase(phrase += ')');
+      helpEndPhrase = false;
+    }
+    setResult(eval(phrase)); // eslint-disable-line no-eval
+    setPhrase = result;
+    setNumber('');
   }
 
   const handleClick = (button) => {
     switch (button) {
       // Number cases add a number to number
-      case 'nine':
-        number += '9';
-        phrase += '9';
-        break;
-      case 'eight':
-        number += '8';
-        phrase += '8';
-        break;
-      case 'seven':
-        number += '7';
-        phrase += '7';
-        break;
-      case 'six':
-        number += '6';
-        phrase += '6';
-        break;
-      case 'five':
-        number += '5';
-        phrase += '5';
-        break;
-      case 'four':
-        number += '4';
-        phrase += '4';
-        break;
-      case 'three':
-        number += '3';
-        phrase += '3';
-        break;
-      case 'two':
-        number += '2';
-        phrase += '2';
-        break;
-      case 'one':
-        number += '1';
-        phrase += '1';
-        break;
-      case 'zero':
-        number += '0';
-        phrase += '0';
+      case '9':
+      case '8':
+      case '7':
+      case '6':
+      case '5':
+      case '4':
+      case '3':
+      case '2':
+      case '1':
+      case '0':
+        setNumber(number += button);
+        setPhrase(phrase += button);
         break;
       case 'clear':
         // Delete everything
-        number = '';
-        phrase = '';
+        setNumber('');
+        setPhrase('');
         break;
       case 'delete':
         // Delete one entry in phrase
-        phrase = phrase.slice(0, -1);
+        setPhrase(phrase.slice(0, -1));
         break;
       case 'exponent':
         // exponent
         if (phrase === '') {
-          number += '0';
-          phrase += '0';
+          setNumber(phrase += '0');
+          setPhrase(phrase += '0');
         }
-        phrase += '**';
-        number = '';
+        setPhrase(phrase += '**');
+        setNumber('');
         break;
       case 'yrootx':
         // yth root
         if (number !== '') {
-          phrase = `Math.pow(${number}, 1/`;
+          setPhrase(phrase += `Math.pow(${number}, 1/`);
           helpEndPhrase = true;
-          number = '';
+          setNumber('');
         }
         break;
       case 'tentothex':
         // Use number to return 10 to its power
-        if (number === '') {
-          number = 0;
-        }
-        phrase = `10**${number}`;
+        addZero();
+        setPhrase(phrase += `10**${number}`);
         evaluate();
         break;
       case 'twotothex':
         // Use number to return 2 to its power
-        if (number === '') {
-          number = 0;
-        }
-        phrase = `2**${number}`;
+        addZero();
+        setPhrase(phrase += `2**${number}`);
         evaluate();
         break;
       case 'log':
         // Log base 10
-        if (number === '') {
-          number = 0;
-        }
-        phrase = `Math.log10(${number})`;
+        addZero();
+        setPhrase(phrase += `Math.log10(${number})`);
         evaluate();
         break;
       case 'logyx':
         // Log base y of x
-        if (number === '') {
-          number = 0;
-        }
-        phrase = `Math.log(${number})/Math.log(10)`;
+        addZero();
+        setPhrase(`Math.log(${number})/Math.log(10)`);
         helpEndPhrase = true;
-        number = '';
+        setNumber = '';
         break;
       case 'naturallog':
         // Log base e
-        if (number === '') {
-          number = 0;
-        }
-        phrase = `Math.log(${number})`;
+        addZero();
+        setPhrase(`Math.log(${number})`);
         evaluate();
         break;
       case 'etothex':
         // Use number to return e to its power
-        if (number === '') {
-          number = 0;
-        }
-        phrase = `e**${number}`;
+        addZero();
+        setPhrase(`e**${number}`);
         evaluate();
         break;
       case 'pi':
         // Get value of pi
-        number = Math.PI();
+        setNumber(Math.PI());
         break;
       case 'e':
         // Get value of e
-        number = Math.E();
+        setNumber(Math.E());
         break;
       case 'absolute':
         // Get absolute value of number
-        if (number === '') {
-          number = 0;
-        }
-        phrase = `Math.abs(${number})`;
+        addZero();
+        setPhrase(phrase + `Math.abs(${number})`);
         evaluate();
         break;
       case 'cube':
         // Cube the number
         if (number !== '') {
-          number = (Number(number) ** 3).toString();
-          result = number;
-          phrase = number;
+          setNumber((Number(number) ** 3).toString());
+          evaluate();
         }
         break;
       case 'leftparen':
@@ -169,111 +137,74 @@ function App() {
         break;
       case 'rightparen':
         // Add a right parenthesis
-        phrase += ')';
+        setPhrase(phrase += ')');
         break;
       case 'percent':
         // Add percent to number
         if (number !== '') {
-          number = (Number(number) / 100).toString();
+          setNumber((Number(number) / 100).toString());
           evaluate();
         }
         break;
       case 'invert':
         // Invert the number
         if (number !== '' && number !== '0') {
-          number = (1 / Number(number)).toString();
+          setNumber((1 / Number(number)).toString());
           evaluate();
         }
         break;
       case 'square':
         // Square the number
         if (number !== '') {
-          number = (Number(number) ** 2).toString();
+          setNumber((Number(number) ** 2).toString());
           evaluate();
         }
         break;
       case 'squareroot':
         // Take the square root
         if (number !== '') {
-          number = Math.sqrt(Number(number)).toString();
+          setNumber(Math.sqrt(Number(number)).toString());
           evaluate();
         }
         break;
       case 'negate':
         // Change positive to negative and vice versa
         if (number !== '') {
-          number = (-Number(number)).toString();
+          setNumber((-Number(number)).toString());
           evaluate();
         }
         break;
-      case 'mod':
+      case '%':
+      case '/':
+      case '*':
+      case '-':
+      case '+':
         // Calculate the modulus (the remainder of a quotient)
         if (phrase === '') {
-          number += '0';
-          phrase += '0';
+          setNumber(number += '0');
+          setPhrase(phrase += '0');
         }
-        phrase += '%';
-        number = ''; // Deleting number after symbol helps decimal checking
-        break;
-      case 'divide':
-        // Division operator
-        if (phrase === '') {
-          number += '0';
-          phrase += '0';
-        }
-        phrase += '/';
-        number = '';
-        break;
-      case 'multiply':
-        // Multiplication operator
-        if (phrase === '') {
-          number += '0';
-          phrase += '0';
-        }
-        phrase += '*';
-        number = '';
-        break;
-      case 'subtract':
-        // Subtraction operator
-        if (phrase === '') {
-          number += '0';
-          phrase += '0';
-        }
-        phrase += '-';
-        number = '';
-        break;
-      case 'add':
-        // Addition operator
-        if (phrase === '') {
-          number += '0';
-          phrase += '0';
-        }
-        phrase += '+';
-        number = '';
+        setPhrase(phrase += button);
+        setNumber(''); // Deleting number after symbol helps decimal checking
         break;
       case 'decimal':
         // Add a decimal if not already there
         if (!number.includes('.')) {
-          number += '.';
+          setNumber += '.';
           phrase += '.';
         }
         break;
       case 'equals':
         // Evaluate the expression
-        /* let cleanPhrase = phrase.replace(/[^-()\d/*+.]/g, '') */; // found this regex here: https://stackoverflow.com/questions/6479236/calculate-string-value-in-javascript-not-using-eval
-        if (helpEndPhrase === true) {
-          phrase += ')';
-        }
         evaluate();
         break;
       default:
-        // Better error handling needed
-        console.log('Somehow you did something I cannot handle');
+        setResult('Error');
         break;
     }
-    console.log(number);
-    console.log(phrase);
-    console.log(result);
+    console.log(`Number: ${number}`);
+    console.log(`Phrase: ${phrase}`);
+    console.log(`Result: ${result}`);
   }
 
   return (
--- src/components/Standard.jsx
+++ src/components/Standard.jsx
@@ -7,32 +7,32 @@ function Standard(props) {
       <Display result={props.result} />
       <div className='keypad'>
         <button className='pad' name='percent' onClick={e => props.handleClick(e.target.name)}>%</button>
-        <button className='pad' name='mod' onClick={e => props.handleClick(e.target.name)}>mod</button>
+        <button className='pad' name='%' onClick={e => props.handleClick(e.target.name)}>mod</button>
         <button className='pad' name='clear' onClick={e => props.handleClick(e.target.name)}>C</button>
         <button className='pad' name='delete' onClick={e => props.handleClick(e.target.name)}>⌫</button><br />
 
         <button className='pad' name='invert' onClick={e => props.handleClick(e.target.name)}><sup>1</sup>/<sub><em>x</em></sub></button>
         <button className='pad' name='square' onClick={e => props.handleClick(e.target.name)}><em>x</em><sup>2</sup></button>
         <button className='pad' name='squareroot' onClick={e => props.handleClick(e.target.name)}>√</button>
-        <button className='pad' name='divide' onClick={e => props.handleClick(e.target.name)}>÷</button><br />
+        <button className='pad' name='/' onClick={e => props.handleClick(e.target.name)}>÷</button><br />
 
-        <button className='pad' name='seven' onClick={e => props.handleClick(e.target.name)}>7</button>
-        <button className='pad' name='eight' onClick={e => props.handleClick(e.target.name)}>8</button>
-        <button className='pad' name='nine' onClick={e => props.handleClick(e.target.name)}>9</button>
-        <button className='pad' name='multiply' onClick={e => props.handleClick(e.target.name)}>×</button><br />
+        <button className='pad' name='7' onClick={e => props.handleClick(e.target.name)}>7</button>
+        <button className='pad' name='8' onClick={e => props.handleClick(e.target.name)}>8</button>
+        <button className='pad' name='9' onClick={e => props.handleClick(e.target.name)}>9</button>
+        <button className='pad' name='*' onClick={e => props.handleClick(e.target.name)}>×</button><br />
 
-        <button className='pad' name='four' onClick={e => props.handleClick(e.target.name)}>4</button>
-        <button className='pad' name='five' onClick={e => props.handleClick(e.target.name)}>5</button>
-        <button className='pad' name='six' onClick={e => props.handleClick(e.target.name)}>6</button>
-        <button className='pad' name='subtract' onClick={e => props.handleClick(e.target.name)}>-</button><br />
+        <button className='pad' name='4' onClick={e => props.handleClick(e.target.name)}>4</button>
+        <button className='pad' name='5' onClick={e => props.handleClick(e.target.name)}>5</button>
+        <button className='pad' name='6' onClick={e => props.handleClick(e.target.name)}>6</button>
+        <button className='pad' name='-' onClick={e => props.handleClick(e.target.name)}>-</button><br />
 
-        <button className='pad' name='one' onClick={e => props.handleClick(e.target.name)}>1</button>
-        <button className='pad' name='two' onClick={e => props.handleClick(e.target.name)}>2</button>
-        <button className='pad' name='three' onClick={e => props.handleClick(e.target.name)}>3</button>
-        <button className='pad' name='add' onClick={e => props.handleClick(e.target.name)}>+</button><br />
+        <button className='pad' name='1' onClick={e => props.handleClick(e.target.name)}>1</button>
+        <button className='pad' name='2' onClick={e => props.handleClick(e.target.name)}>2</button>
+        <button className='pad' name='3' onClick={e => props.handleClick(e.target.name)}>3</button>
+        <button className='pad' name='+' onClick={e => props.handleClick(e.target.name)}>+</button><br />
 
         <button className='pad' name='negate' onClick={e => props.handleClick(e.target.name)}>+/-</button>
-        <button className='pad' name='zero' onClick={e => props.handleClick(e.target.name)}>0</button>
+        <button className='pad' name='0' onClick={e => props.handleClick(e.target.name)}>0</button>
         <button className='pad' name='decimal' onClick={e => props.handleClick(e.target.name)}>.</button>
         <button className='pad' name='equals' onClick={e => props.handleClick(e.target.name)}>=</button><br />
       </div>
