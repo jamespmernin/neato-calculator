6053c25881681642111d9d054dc5bda468f77d3b
James Mernin <jamespmernin@gmail.com>
2020-10-14 10:14:01 -0400

    Added links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import LinkedIn from '../linkedin.png';
+import Github from '../github.png';
 
 function Footer() {
   return (
-    <>
+    <span id="footer">
       <h4>© 2020</h4>
-    </>
+      <a href="https://www.linkedin.com/in/jamesmernin"><img src={LinkedIn} alt="LinkedIn" /></a>
+      <a href="https://www.github.com/jamespmernin"><img src={Github} alt="Github" /></a>
+    </span>
   )
 }
 
